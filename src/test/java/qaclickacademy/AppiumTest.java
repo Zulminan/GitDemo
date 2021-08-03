855b327c78b748be33cd7f9dc7aada2a412b0b05
Zulminan Ahmed <ahmedzulminan@gmail.com>
2021-08-03 12:47:28 +0530

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,23 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	
+	
+	@Test
+	
+	public void nativeAppAndroid()
+	{
+		System.out.println("nativeAppAndroid");
+	}
+	
+@Test
+	
+	public void IOSapps()
+	{
+	System.out.println("IOSapps");
+	}
+	
+}
---
+++ src/test/java/qaclickacademy/RestAPITest.java
@@ -0,0 +1,23 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+
+	
+	
+		@Test
+		public void posJira()
+		{
+			System.out.println("posJira");
+		}
+		
+		@Test
+		public void deleteTwitter()
+		{
+			System.out.println("deleteTwitter");
+		}
+	
+		
+		
+}
